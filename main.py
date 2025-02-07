c981d1fd90e9580d706e109173308456297f43de
Abdullahi Mohammmed Ali <abdumok19@gmail.com>
2025-02-07 21:02:38 +0300

    add score dalculation mechanism and popup window showing how to play

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ window= Screen()
 window.bgcolor("black")
 window.setup(width= 1000, height= 600)
 window.title("Paddle Game v1.00")
+window.textinput(title="How To Play", prompt="1- Square => 1 Point\n2- Circle => 3 Points"
+                                            "\n3- Triangle => Reset the Score\n4- Turtle => Game Over")
 window.tracer(0)
 
 # initialize the paddle:
@@ -34,16 +36,27 @@ while game_on:
         food.create_food()
 
     for part in paddle.all_parts:
+        # collision with paddle and the shape was turtle:
         if part.distance(food) < 15:
             if food.shape() == "turtle" :
                 food.hide()
                 window.bgcolor("red")
                 score.game_over()
                 game_on= False
+            # collision with paddle:
             else:
-                score.increase(2)
+                # the shape is triangle: reset the score:
+                if food.shape() == "triangle":
+                    score.score =0
+                # the shape is triangle: add 1 point:
+                elif food.shape() == "square":
+                    score.increase(1)
+                # the shape is circle: add 3 point:
+                elif food.shape() == "circle":
+                    score.increase(3)
                 food.hide()
                 food.create_food()
+                print(food.shape())
 
 
 
